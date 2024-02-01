4e10576451103fbae49cc4c6f6609c59ef07722a
MayHyeyeonKim <hkim7963@gmail.com>
2024-02-01 00:09:21 -0800

    chore

---
+++ Application.java
@@ -0,0 +1,13 @@
+package Seattle.seahawks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
